ba136fe4dc5874c9af88109ec2eff9903ff60dcb
daddkumar <kirankumardaddala@gmail.com>
2023-09-19 00:18:26 +0530

    Created springboot application

---
+++ src/main/java/com/newjava/springboot/studentrecords/StudentRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.newjava.springboot.studentrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newjava/springboot/studentrecords/StudentdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newjava.springboot.studentrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
